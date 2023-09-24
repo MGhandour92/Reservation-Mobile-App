24f3c437b385a771b03c8916863ba021621a37c4
Mohamed El Ghandour <m.el_ghandour@hotmail.com>
2023-09-24 23:46:15 +0300

    Reservation without maintenance

--- FirstAppMaui/Common/Entities/Reservation.cs
+++ FirstAppMaui/Common/Entities/Reservation.cs
@@ -11,13 +11,15 @@ namespace Common.Entities
 	{
         public int Id { get; set; }
         [Required]
-        public DateTime ReservationDate { get; set; }
+        public DateTime ReservationDate { get; set; } = DateTime.Today.AddDays(1);
 
 
         [Required]
+        [Range(1, int.MaxValue, ErrorMessage = "Please a Customer")]
         public int CustomerId { get; set; }
 		public Customer? Customer { get; set; }
         [Required]
+        [Range(1, int.MaxValue, ErrorMessage = "Please a Car")]
         public int CarId { get; set; }
 		public Car? Car { get; set; }
 
